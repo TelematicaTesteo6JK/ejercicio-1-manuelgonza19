12b6bf75ed0c57da71c31fd18c2b38bfcaf58360
manuelgonza19 <vgonzalez5@ucol.mx>
2017-03-24 23:41:28 -0600

    ejercicio1
    rama del ejercicio1

--- pago_estacionamiento.cpp
+++ pago_estacionamiento.cpp
@@ -23,7 +23,16 @@ float pago_estacionamiento(int horas)
 void casos_de_prueba(){
 
     cout << "TEST CASES" << endl;
-    // Agrega tus casos de prueba
+    cout <<"test data/-1/ expected result $0/ <<pago_estacionamiento;
+    cout <<"test data/3/ expected result $30/ <<pago_estacionamiento;
+    cout <<"test data/8/ expected result $72/ <<pago_estacionamiento;
+    cout <<"test data/12/ expected result $84/ <<pago_estacionamiento;
+    cout <<"test data/0/ expected result $0/ <<pago_estacionamiento;
+    cout <<"test data/1/ expected result $10/ <<pago_estacionamiento;
+    cout <<"test data/5/ expected result $50/ <<pago_estacionamiento;
+    cout <<"test data/6/ expected result $54/ <<pago_estacionamiento;
+    cout <<"test data/10/ expected result $70/ <<pago_estacionamiento;
+    cout <<"test data/11/ expected result $77/ <<pago_estacionamiento;
 
 
 
